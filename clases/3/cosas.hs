f6574a6f0f9011f90a40eee891faad3b14b436ac
Agustin Borgna <hola@aborgna.com.ar>
2015-04-25 16:18:58 -0300

    class 3

---
+++ clases/3/cosas.hs
@@ -0,0 +1,27 @@
+
+listar :: a -> a -> a -> [a]
+listar a b c = [a,b,c]
+
+rangoDePaso :: (Enum t, Num t) => t -> t -> t -> [t]
+rangoDePaso a z b = [a,a+b..z]
+
+fib :: Int -> Integer
+fib  = (fibs!!)
+
+fibs :: [Integer]
+fibs = 1 : 1 : zipWith (+) (fibs) (tail fibs)
+
+reverso :: [a] -> [a]
+reverso = foldl (flip (:)) []
+
+capicua :: Eq a => [a] -> Bool
+capicua xs = xs == reverso xs
+
+productoria :: [Integer] -> Integer
+productoria = foldr (*) 1
+
+comb :: Integral a => a -> a -> a
+comb n m | m == 0 = 1
+         | n == m = 1
+         | n < m = error "n es menor a m"
+         | otherwise = comb (n-1) (m-1) + comb (n-1) m
