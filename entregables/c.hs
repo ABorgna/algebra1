5d40a4ff7fffc53f210a0e50819a8b1cf054b9eb
Agustin Borgna <hola@aborgna.com.ar>
2015-06-22 20:24:08 -0300

    entregables

--- entregables/c.hs
+++ entregables/c.hs
@@ -1,23 +1,27 @@
-import Data.List (find)
+import Control.Monad (filterM)
+import Data.List (find,group,nub,tails)
 import Data.Maybe (fromJust)
 
--- | El triangulo de lados a b c es rectangulo?
-esPitagorica :: (Ord a, Floating a) => a -> a -> a -> Bool
+-- | 1.5
+-- El triangulo de lados a b c es rectangulo?
+esPitagorica :: (Eq a, Floating a) => a -> a -> a -> Bool
 esPitagorica a b c = a**2 * b**2 == c**2
 
--- | Pendiente de la recta entre los puntos
-pendiente :: Floating a => (a,a) -> (a,a) -> a
+-- | 2.2
+-- Pendiente de la recta entre los puntos
+pendiente :: (Float,Float) -> (Float,Float) -> Float
 pendiente (x0,y0) (x1,y1) = (y1-y0) / (x1-x0)
 
--- |
-sumaImparesCuadradosMenoresA :: Integer -> Integer
-sumaImparesCuadradosMenoresA n = sum [1,3.. floor $ sqrt $ fromIntegral n]
+-- | 3.4
+sumaImparesCuadradosMenoresA :: Integral a => a -> a
+sumaImparesCuadradosMenoresA n = sum $ takeWhile (\x -> x^2 < n) [1,3..]
 
--- |
+-- | 4.2
 prodInterno :: [Float] -> [Float] -> Float
 prodInterno = fmap sum . zipWith (*)
 
--- |
+-- | 5.5
+-- La consigna no especifica el criterio de ordenamiento
 isOrderedBy :: (a -> a -> Bool) -> [a] -> Bool
 isOrderedBy cmp xs@(x:y:_) = x `cmp` y && isOrderedBy cmp (tail xs)
 isOrderedBy _ _ = True
@@ -25,26 +29,32 @@ isOrderedBy _ _ = True
 isOrdered :: Ord a => [a] -> Bool
 isOrdered = isOrderedBy (<=)
 
--- |
+-- | 7.4
 primes :: [Integer]
 primes = 2 : filter isPrime [3,5..]
+    where isPrime p = let s = floor $ sqrt $ fromIntegral p
+                      in not $ any (divides p) $ takeWhile (<= s) primes
 
-isPrime :: Integer -> Bool
-isPrime p = not $ any (divides p) $ takeWhile (<=s) primes
-    where s = floor $ sqrt $ fromIntegral p
+factorizar :: Integer -> [Integer]
+factorizar 1 = []
+factorizar n =  f : factorizar (n `quot` f)
+    where f = fromJust $ find (divides n) primes
 
 divides :: Integer -> Integer -> Bool
 divides n m = n `rem` m == 0
 
-factorize :: Integer -> [Integer]
-factorize 1 = []
-factorize n =  f : factorize (n `div` f)
-    where f = fromJust $ find (divides n) primes
-
--- |
+-- | 8.2.4
 data Lista a = Vacia | Agregar a (Lista a)
 
 iguales :: Eq a => Lista a -> Lista a -> Bool
 iguales Vacia Vacia = True
-iguales (a:as) (b:bs) = a == b || iguales as bs
+iguales (Agregar a as) (Agregar b bs) = a == b || iguales as bs
 iguales _ _ = False
+
+-- | 9.5
+divisores :: Integer -> [Integer]
+divisores = map (product . concat) . mapM tails . group . factorizar
+
+seisDivisores :: [Integer]
+seisDivisores = filter ((==6) . length . divisores) [1..1000]
+
--- entregables/c1.hs
+++
@@ -1,6 +0,0 @@
-import Data.List (sort)
-
--- | El triangulo de lados a b c es rectangulo?
-esPitagorica :: (Ord a, Floating a) => a -> a -> a -> Bool
-esPitagorica a b c = let (d:f:h:[]) = map (**2) $ sort [a,b,c] in h == d+f
-
