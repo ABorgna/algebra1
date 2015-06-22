8d38f4b9a215e6f57e746e5a467164958dc0ac11
Agustin Borgna <hola@aborgna.com.ar>
2015-06-22 18:12:37 -0300

    Entregables

--- entregables/c.hs
+++ entregables/c.hs
@@ -1,3 +1,5 @@
+import Data.List (find)
+import Data.Maybe (fromJust)
 
 -- | El triangulo de lados a b c es rectangulo?
 esPitagorica :: (Ord a, Floating a) => a -> a -> a -> Bool
@@ -17,8 +19,32 @@ prodInterno = fmap sum . zipWith (*)
 
 -- |
 isOrderedBy :: (a -> a -> Bool) -> [a] -> Bool
-isOrderedBy cmp xs@(x:y:_) = x `cmp` y && isOrdered cmp (tail xs)
+isOrderedBy cmp xs@(x:y:_) = x `cmp` y && isOrderedBy cmp (tail xs)
 isOrderedBy _ _ = True
 
 isOrdered :: Ord a => [a] -> Bool
 isOrdered = isOrderedBy (<=)
+
+-- |
+primes :: [Integer]
+primes = 2 : filter isPrime [3,5..]
+
+isPrime :: Integer -> Bool
+isPrime p = not $ any (divides p) $ takeWhile (<=s) primes
+    where s = floor $ sqrt $ fromIntegral p
+
+divides :: Integer -> Integer -> Bool
+divides n m = n `rem` m == 0
+
+factorize :: Integer -> [Integer]
+factorize 1 = []
+factorize n =  f : factorize (n `div` f)
+    where f = fromJust $ find (divides n) primes
+
+-- |
+data Lista a = Vacia | Agregar a (Lista a)
+
+iguales :: Eq a => Lista a -> Lista a -> Bool
+iguales Vacia Vacia = True
+iguales (a:as) (b:bs) = a == b || iguales as bs
+iguales _ _ = False
